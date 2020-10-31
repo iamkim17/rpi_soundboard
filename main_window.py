b32b36d4e98c54e79e7200ec6217b6544f373b24
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-10-31 17:55:44 +0100

    Volume slider now resizes when window size is changed.

--- main_window.py
+++ main_window.py
@@ -41,35 +41,57 @@ class MainWindow(Tk.Frame):
         self.sound_file_path_vars = [Tk.StringVar() for i in range(self.model.get_num_sounds())] 
         self.sound_pin_name_vars = [Tk.StringVar() for i in range(self.model.get_num_sounds())]
 
-        # Parent widget for the buttons
-        buttons_frame = Tk.Frame(self.root)
-        buttons_frame.grid(row=0, column=0, sticky=Tk.W+Tk.E)    
-
-
-        btn_Image = Tk.Button(buttons_frame, text='Image')
-        btn_Image.grid(row=0, column=0, padx=(10), pady=10)
-
-        btn_File = Tk.Button(buttons_frame, text='File')
-        btn_File.grid(row=0, column=1, padx=(10), pady=10)
-
-        btn_Folder = Tk.Button(buttons_frame, text='Folder')
-        btn_Folder.grid(row=0, column=2, padx=(10), pady=10)
-
-
-
-        group1 = Tk.LabelFrame(self.root, text="Text Box", padx=5, pady=5)
-        group1.grid(row=1, column=0, columnspan=3, padx=10, pady=10, sticky=Tk.E+Tk.W+Tk.N+Tk.S)
-
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
 
-        group1.rowconfigure(0, weight=1)
-        group1.columnconfigure(0, weight=1)
+        sounds_frame = Tk.Frame(self.root)
+        sounds_frame.grid(row=0, column=0, sticky=Tk.E+Tk.W+Tk.N+Tk.S)
+
+        sounds_frame.columnconfigure(0, weight=0)
+        sounds_frame.columnconfigure(1, weight=1)
+        sounds_frame.columnconfigure(2, weight=0)
+
+        self.sound_file_path_vars = [Tk.StringVar() for i in range(self.model.get_num_sounds())] 
+        self.sound_pin_name_vars = [Tk.StringVar() for i in range(self.model.get_num_sounds())]
+        self.sound_pin_option_menus = []
+
+        #group1 = Tk.Frame(self.root, padx=5, pady=5)
+        #group1.grid(row=1, column=0, columnspan=3, sticky=Tk.E+Tk.W+Tk.N+Tk.S)
+
+        #group1.rowconfigure(0, weight=1)
+        #group1.columnconfigure(0, weight=1)
 
         # Create the textbox
-        txtbox = scrolledtext.ScrolledText(group1, width=40, height=10)
-        txtbox.grid(row=0, column=0,   sticky=Tk.E+Tk.W+Tk.N+Tk.S)
+        sound_number = 1
+        row_counter = 0
 
+        # sound number
+        sound_label= Tk.Label(sounds_frame, text="Sound " + str(sound_number), justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter, column=0, columnspan=3)
+
+        # sound file selection
+        sound_file_label = Tk.Label(sounds_frame, text="File", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=1, column=0)
+
+        self.sound_file_path_vars[sound_number-1].set(self.model.get_file_path_for_sound(sound_number))
+        sound_file_path_entry = Tk.Entry(sounds_frame, textvariable=self.sound_file_path_vars[sound_number-1], state='readonly').grid(sticky=Tk.W+Tk.E, row=1, column=1)
+        sound_choose_file_button = Tk.Button(sounds_frame, text="Open", command=lambda sound_number=sound_number: self.on_choose_sound_file(sound_number)).grid(sticky=Tk.W, row=row_counter+1, column=2)
+
+        # GPIO pin selection
+        sound_pin_label = Tk.Label(sounds_frame, text="Pin", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+2, column=0)
+        self.sound_pin_name_vars[sound_number-1].set(self.model.get_pin_for_sound(sound_number))
+
+        sound_number_copy=sound_number
+
+        sound_pin_option_menu = Tk.OptionMenu(sounds_frame, self.sound_pin_name_vars[sound_number-1], *model.get_unassigned_gpio_pin_names(), command=lambda pin_name=sound_number, sound_number=sound_number_copy: self.on_choose_sound_pin(pin_name, sound_number)).grid(sticky=Tk.W, row=row_counter+2, column=1)
+
+        # volume slider 
+        sound_volume_label = Tk.Label(sounds_frame, text="Vol.", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W+Tk.S, row=3, column=0)
+        sound_volume_slider_var = Tk.IntVar()
+        sound_volume_slider_var.set(100*self.model.get_volume_for_sound(sound_number))
+
+        sound_volume_slider = Tk.Scale(sounds_frame, variable=sound_volume_slider_var, command=lambda volume=sound_number, sound_number=sound_number_copy: self.on_volume_change(volume, sound_number), from_=0, to_=100, orient=Tk.HORIZONTAL).grid(sticky=Tk.W+Tk.E, row=row_counter+3, column=1)
+
+        # play button
+        sound_play_button = Tk.Button(sounds_frame, text="Play", command=lambda sound_number=sound_number: self.on_play_sound(sound_number)).grid(sticky=Tk.W+Tk.E+Tk.S, row=row_counter+3, column=2)
 
 
         """
