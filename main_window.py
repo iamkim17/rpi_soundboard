24c6dbf2a3cc10ab1a5a941cfe9a259a3b188e88
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-01 10:48:59 +0100

    Added sounds to FrameWithScrollBar.

--- main_window.py
+++ main_window.py
@@ -20,7 +20,7 @@ class MainWindow(Tk.Frame):
         self.model.register_observer(self)
 
         self.root = root
-        self.root.minsize(395, 266)
+        self.root.minsize(395, 372)
 
         Tk.Frame.__init__(self, self.root)
 
@@ -54,57 +54,49 @@ class MainWindow(Tk.Frame):
         self.sound_pin_option_menus = []
 
         sounds_frame = FrameWithScrollBar(self.root)
-        sounds_frame.frame.columnconfigure(0, weight=1)
-
-        for i in range(30):
-            Tk.Entry(sounds_frame.frame).grid(row=i, column=0, sticky=Tk.W+Tk.E)
+        sounds_frame.frame.columnconfigure(1, weight=1)
 
         sounds_frame.pack(expand=True, fill=Tk.BOTH)
 
-
-
-        
-        """
         row_counter = 0
 
         for sound_number in range(1,self.model.get_num_sounds()+1):
             # sound number
-            sound_label= Tk.Label(sounds_frame.scrollable_frame, text="Sound " + str(sound_number), justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+0, column=0, columnspan=3)
+            sound_label= Tk.Label(sounds_frame.frame, text="Sound " + str(sound_number), justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+0, column=0, columnspan=3)
 
             # sound file selection
-            sound_file_label = Tk.Label(sounds_frame.scrollable_frame, text="File", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+1, column=0)
+            sound_file_label = Tk.Label(sounds_frame.frame, text="File", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+1, column=0)
 
             self.sound_file_path_vars[sound_number-1].set(self.model.get_file_path_for_sound(sound_number))
-            sound_file_path_entry = Tk.Entry(sounds_frame.scrollable_frame, textvariable=self.sound_file_path_vars[sound_number-1], state='readonly').grid(sticky=Tk.W+Tk.E, row=row_counter+1, column=1)
-            sound_choose_file_button = Tk.Button(sounds_frame.scrollable_frame, text="Open", command=lambda sound_number=sound_number: self.on_choose_sound_file(sound_number)).grid(sticky=Tk.W, row=row_counter+1, column=2)
+            sound_file_path_entry = Tk.Entry(sounds_frame.frame, textvariable=self.sound_file_path_vars[sound_number-1], state='readonly').grid(sticky=Tk.W+Tk.E, row=row_counter+1, column=1)
+            sound_choose_file_button = Tk.Button(sounds_frame.frame, text="Open", command=lambda sound_number=sound_number: self.on_choose_sound_file(sound_number)).grid(sticky=Tk.W, row=row_counter+1, column=2)
 
             # GPIO pin selection
-            sound_pin_label = Tk.Label(sounds_frame.scrollable_frame, text="Pin", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+2, column=0)
+            sound_pin_label = Tk.Label(sounds_frame.frame, text="Pin", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W, row=row_counter+2, column=0)
             self.sound_pin_name_vars[sound_number-1].set(self.model.get_pin_for_sound(sound_number))
 
             sound_number_copy=sound_number
 
-            sound_pin_option_menu = Tk.OptionMenu(sounds_frame.scrollable_frame, self.sound_pin_name_vars[sound_number-1], *model.get_unassigned_gpio_pin_names(), command=lambda pin_name=sound_number, sound_number=sound_number_copy: self.on_choose_sound_pin(pin_name, sound_number)).grid(sticky=Tk.W, row=row_counter+2, column=1)
+            sound_pin_option_menu = Tk.OptionMenu(sounds_frame.frame, self.sound_pin_name_vars[sound_number-1], *model.get_unassigned_gpio_pin_names(), command=lambda pin_name=sound_number, sound_number=sound_number_copy: self.on_choose_sound_pin(pin_name, sound_number)).grid(sticky=Tk.W, row=row_counter+2, column=1)
 
             # volume slider 
-            sound_volume_label = Tk.Label(sounds_frame.scrollable_frame, text="Vol.", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W+Tk.S, row=3, column=0)
+            sound_volume_label = Tk.Label(sounds_frame.frame, text="Vol.", justify=Tk.LEFT, anchor="w").grid(sticky=Tk.W+Tk.S, row=3, column=0)
             sound_volume_slider_var = Tk.IntVar()
             sound_volume_slider_var.set(100*self.model.get_volume_for_sound(sound_number))
 
-            sound_volume_slider = Tk.Scale(sounds_frame.scrollable_frame, variable=sound_volume_slider_var, command=lambda volume=sound_number, sound_number=sound_number_copy: self.on_volume_change(volume, sound_number), from_=0, to_=100, orient=Tk.HORIZONTAL).grid(sticky=Tk.W+Tk.E, row=row_counter+3, column=1)
+            sound_volume_slider = Tk.Scale(sounds_frame.frame, variable=sound_volume_slider_var, command=lambda volume=sound_number, sound_number=sound_number_copy: self.on_volume_change(volume, sound_number), from_=0, to_=100, orient=Tk.HORIZONTAL).grid(sticky=Tk.W+Tk.E, row=row_counter+3, column=1)
 
             # play button
-            sound_play_button = Tk.Button(sounds_frame.scrollable_frame, text="Play", command=lambda sound_number=sound_number: self.on_play_sound(sound_number)).grid(sticky=Tk.W+Tk.E+Tk.S, row=row_counter+3, column=2)
+            sound_play_button = Tk.Button(sounds_frame.frame, text="Play", command=lambda sound_number=sound_number: self.on_play_sound(sound_number)).grid(sticky=Tk.W+Tk.E+Tk.S, row=row_counter+3, column=2)
 
             # dont display last separator
             if sound_number != self.model.get_num_sounds():
-                sound_separator = ttk.Separator(sounds_frame.scrollable_frame, orient=Tk.HORIZONTAL).grid(sticky=Tk.W+Tk.E, row=row_counter+4, column=0, columnspan=3, pady=5)
+                sound_separator = ttk.Separator(sounds_frame.frame, orient=Tk.HORIZONTAL).grid(sticky=Tk.W+Tk.E, row=row_counter+4, column=0, columnspan=3, pady=5)
 
 
             row_counter = row_counter + 5
 
-        """
-
+        
         self.root.update()   
         print(self.root.winfo_width(), self.root.winfo_height())
 
