716b52b1d2e01a07aaf85fcc54ec5b379cd7b1a5
Konstantin Luebeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-01 10:39:54 +0100

    New attempt with scrollbar on frame.

---
+++ frame_with_scroll_bar.py
@@ -0,0 +1,35 @@
+# adapted from: https://stackoverflow.com/a/57745179/2096060
+
+try:
+    import Tkinter as Tk
+    from Tkinter import ttk
+except ModuleNotFoundError:
+    import tkinter as Tk
+    from tkinter import ttk
+
+class FrameWithScrollBar(Tk.Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.canvas = Tk.Canvas(self, bg='yellow')
+        self.frame = Tk.Frame(self.canvas, bg='green')
+        self.scrollbar = Tk.Scrollbar(self, orient='vertical',
+                                command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+        self.canvas.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=True)
+        self.scrollbar.pack(side=Tk.RIGHT, fill=Tk.BOTH)
+        self.frame.pack(fill=Tk.BOTH, expand=True)
+        self._frame_id = self.canvas.create_window(
+                                 self.canvas.winfo_width(), 0,
+                                 anchor='nw',
+                                 window=self.frame)
+        self.frame.bind('<Configure>', self.onFrameConfigure)
+        self.canvas.bind('<Configure>', self.onCanvasConfigure)
+
+    def onFrameConfigure(self, event):       
+        self.canvas.configure(scrollregion=self.frame.bbox('all'))
+
+    def onCanvasConfigure(self, event):
+        width = event.width
+        self.canvas.itemconfigure(self._frame_id, width=self.canvas.winfo_width())
+
--- main_window.py
+++ main_window.py
@@ -11,7 +11,7 @@ except ModuleNotFoundError:
 
 from consts import *
 from about_dialog import AboutDialog
-from scrollable_frame import ScrollableFrame
+from frame_with_scroll_bar import FrameWithScrollBar
 
 class MainWindow(Tk.Frame):
     def __init__(self, model, root):
@@ -53,13 +53,24 @@ class MainWindow(Tk.Frame):
         self.sound_pin_name_vars = [Tk.StringVar() for i in range(self.model.get_num_sounds())]
         self.sound_pin_option_menus = []
 
+        fws = FrameWithScrollBar(self.root)
+        fws.frame.columnconfigure(0, weight=1)
+
+        for i in range(30):
+            Tk.Entry(fws.frame).grid(row=i, column=0, sticky=Tk.W+Tk.E)
+
+        fws.pack(expand=True, fill=Tk.BOTH)
+
+
+
+        """
         f = Tk.Frame(self.root) 
         f.grid(row=0, column=0, sticky=Tk.N+Tk.W+Tk.E+Tk.S)
 
         canvas = Tk.Canvas(f, bg="red")
         scrollbar = ttk.Scrollbar(f, orient="vertical", command=canvas.yview)
 
-        scrollable_frame = ttk.Frame(canvas)
+        scrollable_frame = Tk.Frame(canvas)
 
         scrollable_frame.bind(
             "<Configure>",
@@ -69,15 +80,18 @@ class MainWindow(Tk.Frame):
         )
 
         canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
-
         canvas.configure(yscrollcommand=scrollbar.set)
 
         canvas.pack(side="left", fill="both", expand=True)
         scrollbar.pack(side="right", fill="y")
 
-        for i in range(50):
-            Tk.Label(scrollable_frame, text="test").grid(row=i, column=0)
+        scrollable_frame.columnconfigure(0, weight=1)
 
+        for i in range(50):
+            Tk.Entry(scrollable_frame).grid(row=i, column=0, sticky=Tk.W+Tk.E)
+
+
+        """
         """
         canvas = Tk.Canvas(self.root, bg="red")
         scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
@@ -99,8 +113,7 @@ class MainWindow(Tk.Frame):
 
         for i in range(20):
             Tk.Label(sf.scrollable_frame, text="test" + str(i)).grid(row=i, column=0)
-        """
-        """
+
         row_counter = 0
 
         for sound_number in range(1,self.model.get_num_sounds()+1):
