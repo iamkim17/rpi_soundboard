d619bcea475523b6762640e562a1e9eef1df4ffe
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-10 18:43:28 +0000

    Added volume to ffplay in Sound.

--- sound.py
+++ sound.py
@@ -3,8 +3,14 @@ import subprocess
 class Sound():
     def __init__(self, model):
         self.model = model
+        self.p = None
 
     def play(self, sound_number):
-        #pygame.mixer.music.load(self.model.get_file_path_for_sound(sound_number))
-        #pygame.mixer.music.set_volume(self.model.get_volume_for_sound(sound_number))
-        subprocess.call(["ffplay", "-nodisp", "-autoexit", "-volume", "80", self.model.get_file_path_for_sound(sound_number)])
+        if self.p is None:
+            self.p = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-volume", str(self.model.get_volume_for_sound(sound_number)*100), self.model.get_file_path_for_sound(sound_number)])
+        else:
+            self.p.terminate()
+            self.p = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-volume", str(self.model.get_volume_for_sound(sound_number)*100), self.model.get_file_path_for_sound(sound_number)])
+
+        
+
