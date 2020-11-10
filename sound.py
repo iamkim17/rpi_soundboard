043bf89a3a8dbacfaae1e5558a8a8a867bc369bf
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-10 17:16:33 +0000

    Replaced pygame with ffplay subprocess.

--- sound.py
+++ sound.py
@@ -1,11 +1,10 @@
-import pygame
+import subprocess
 
 class Sound():
     def __init__(self, model):
         self.model = model
-        pygame.mixer.init()
 
     def play(self, sound_number):
-        pygame.mixer.music.load(self.model.get_file_path_for_sound(sound_number))
-        pygame.mixer.music.set_volume(self.model.get_volume_for_sound(sound_number))
-        pygame.mixer.music.play()
+        #pygame.mixer.music.load(self.model.get_file_path_for_sound(sound_number))
+        #pygame.mixer.music.set_volume(self.model.get_volume_for_sound(sound_number))
+        subprocess.call(["ffplay", "-nodisp", "-autoexit", "-volume", "80", self.model.get_file_path_for_sound(sound_number)])
