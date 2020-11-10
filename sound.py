62eb5878f9c980e802a242e3507d961936be07f7
Konstantin Lübeck <konstantin.luebeck@uni-tuebingen.de>
2020-11-10 18:47:00 +0000

    Refactored Sound.

--- sound.py
+++ sound.py
@@ -7,10 +7,11 @@ class Sound():
 
     def play(self, sound_number):
         if self.p is None:
-            self.p = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-volume", str(self.model.get_volume_for_sound(sound_number)*100), self.model.get_file_path_for_sound(sound_number)])
+            self.subprocess_init(sound_number)
         else:
             self.p.terminate()
-            self.p = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-volume", str(self.model.get_volume_for_sound(sound_number)*100), self.model.get_file_path_for_sound(sound_number)])
+            self.subprocess_init(sound_number)
 
-        
+    def subprocess_init(self, sound_number):
+        self.p = subprocess.Popen(["ffplay", "-nodisp", "-autoexit", "-volume", str(self.model.get_volume_for_sound(sound_number)*100), self.model.get_file_path_for_sound(sound_number)])
 
